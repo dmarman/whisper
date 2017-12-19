e62c41c2e5bb30b1ac5d6edd94cd9e4a0c0b2ca4
dmarman <dmm.martin@gmail.com>
2017-12-19 14:09:02 +0100

    add random background images

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?php 
+$bg = array('1.jpg', '2.jpg', '3.jpg', '4.jpg'); // array of filenames
+
+  $i = rand(0, count($bg)-1); // generate random number size of the array
+  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
@@ -5,6 +11,16 @@
 		<meta name="viewport" content="width=device-width">
 		<title><?php bloginfo('name'); ?></title>
 		<?php wp_head(); ?>
+
+		<style>	
+			body {
+				background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(/whisper/wp-content/themes/whisper/img/<?php echo  $selectedBg;?>) no-repeat center center fixed;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+			}
+		</style>
 	</head>
 	
 <body <?php body_class(); ?>>
