6f877ccf597eb2d59b616a81f9d94f759e6f75cf
Domingo <dmm.martin@gmail.com>
2018-01-21 11:23:20 +0100

    yolo

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php 
-$bg = array('1.jpg', '2.jpg', '3.jpg', '4.jpg'); // array of filenames
+$bg = array('1.jpeg', '2.jpeg'); // array of filenames
 
   $i = rand(0, count($bg)-1); // generate random number size of the array
   $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
@@ -10,11 +10,12 @@ $bg = array('1.jpg', '2.jpg', '3.jpg', '4.jpg'); // array of filenames
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width">
 		<title><?php bloginfo('name'); ?></title>
+        <link rel="icon" href="http://whispernotagency.com/wp-content/themes/whisper/favicon.png?v=3" />
 		<?php wp_head(); ?>
 
 		<style>	
 			body {
-				background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(/whisper/wp-content/themes/whisper/img/<?php echo  $selectedBg;?>) no-repeat center center fixed;
+				background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(/wp-content/themes/whisper/img/<?php echo  $selectedBg;?>) no-repeat center center fixed;
 				-webkit-background-size: cover;
 				-moz-background-size: cover;
 				-o-background-size: cover;
@@ -27,4 +28,9 @@ $bg = array('1.jpg', '2.jpg', '3.jpg', '4.jpg'); // array of filenames
 	<div class="container">
 		<header class="site-header">
 			<h1 class="title"><a href="<?php echo home_url(); ?>">Whis<br>per&nbsp;&nbsp;&nbsp;Not<br>Ag<br><span class="title-component">e&nbsp;&nbsp;&nbsp;&nbsp;ncy</span></a></h1>
+            <div class="social">
+                <p><a target="_blank" href="https://www.facebook.com/whispernotagency/">Facebook</a></p>
+                <p><a target="_blank" href="https://www.instagram.com/whispernotagency/">Instagram</a></p>
+                <p><a target="_blank" href="https://twitter.com/whispernot_?lang=ca">Twitter</a></p>
+            </div>
 		</header>
